1101045854397fcc5ee92ac70df8cb253adb4b01
aracelly <aracellybootcamp@gmail.com>
2021-10-18 22:11:32 -0300

    se ajusta pixeles para img  card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <v-card  class="mx-auto my-12" max-width="320">
+    <v-card  class="mx-auto my-12" max-width="300">
       <template slot="progress">
         <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
       </template>
 
-      <v-img height="270" :src="urlImg"></v-img>
+      <v-img height="300" :src="urlImg"></v-img>
 
       <v-card-title>{{name}}</v-card-title>
 
